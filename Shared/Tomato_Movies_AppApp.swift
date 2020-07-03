7331f6a62a83f9f4f2e51e6208e7b6a7c01bd685
dinesh10c04@gmail.com <dinesh.adhithya@zohocorp.com>
2020-07-04 00:42:48 +0530

    Initial Commit

---
+++ Shared/Tomato_Movies_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tomato_Movies_AppApp.swift
+//  Shared
+//
+//  Created by Dineshadhithya V on 04/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Tomato_Movies_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
